6f86f54455eacaaaca9a086fff9d83357900ee21
mohitkothari-dev <mrulekothari@gmail.com>
2024-04-18 22:15:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ParkinsonGPT
+
+### This is a chatbot application developed for an AI hackathon. Its primary purpose is to raise awareness about Parkinson's disease. 
+
+It can be quite overwhelming to go through online content such as research papers and videos related to Parkinson's disease. There are many medical terms and concepts to understand, and it takes time to grasp the exact nature of the disease. To simplify this process, I developed ParkinsonGPT - a RAG chatbot that utilizes cutting-edge AI technology to provide a one-stop solution for individuals seeking information on Parkinson's disease.
---
+++ src/utils/OpenAi.js
@@ -0,0 +1,30 @@
+
+export const sendMsgToAI = async (msg) => {
+    // Remove OpenAI specific details
+  
+    const API_URL = process.env.REACT_APP_API_KEY; // Replace with your actual API endpoint URL
+    const apiKey = process.env.REACT_APP_BEARER_TOKEN; // Replace with your actual API key (consider environment variables)
+  
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ question: msg })
+      });
+  
+      const responseData = await response.json();
+  
+      // Handle response based on your API's structure
+      if (responseData && responseData.text) { // Replace "yourDataField" with the actual field containing the response text
+        return responseData.text;
+      } else {
+        console.error('Error: Your API response did not contain a valid response field.');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+    
